2fd83f5f579363014c86ae8fb42b6d92103b2a08
carlosfabianchaves <cchaves@isthmusit.com>
2020-04-24 16:49:53 -0600

    adding prod

---
+++ build/precache-manifest.629e666d05da7e927c64dc6b7d586270.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b033728e987886c7d578ea454315ad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a6266202e4a792fe78e",
+    "url": "/static/css/main.1b5d5983.chunk.css"
+  },
+  {
+    "revision": "4b77b34833e1a04140ff",
+    "url": "/static/js/2.adce48bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.adce48bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a6266202e4a792fe78e",
+    "url": "/static/js/main.6a6410cd.chunk.js"
+  },
+  {
+    "revision": "aa397f78cc781881d640",
+    "url": "/static/js/runtime-main.d841c0e7.js"
+  },
+  {
+    "revision": "1053dab831ed7a48a67ff7771ea7ccd9",
+    "url": "/static/media/hero-home.1053dab8.jpg"
+  }
+]);
